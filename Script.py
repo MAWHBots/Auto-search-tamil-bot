d2ce4a4c5d7f97b7e44d7d53ac56f04baba57a8b
MAWHBots <160210473+MAWHBots@users.noreply.github.com>
2024-07-09 20:40:03 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -198,7 +198,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ:[@mawhOrzzBackUp2] {file_name}
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
 ▫️<a href=https://t.me/moviesandwebserieshubOrzz> ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ </a>
 ▫️<a href=https://t.me/+TKCsEgFK7TA3NDE1> ʀᴇǫᴜᴇsᴛ ᴍᴏᴠɪᴇs​ </a>
